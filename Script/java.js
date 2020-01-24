526a0c4424091e87e6d5a8c62267f4f97b374daf
Will Coan <wcoan300@gmail.com>
2020-01-24 18:19:26 -0500

    added backgrounds

--- Script/java.js
+++ Script/java.js
@@ -13,7 +13,7 @@ function showSlides() {
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 4000); 
+  
 }
 
 const someElement = document.querySelector(".nav");
